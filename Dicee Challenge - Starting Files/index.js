740aa0e6a3807853294f711f4f9f3c86acfb61bb
Shubham <shubhamsingh705010@gmail.com>
2023-10-22 16:56:50 +0530

    hurrey game completed

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -3,3 +3,15 @@ var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
 document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
 
+function result() {
+    var selected = document.querySelector("h1");
+    if (randomNumber1 === randomNumber2) {
+        selected.innerHTML = "Draw!";
+    } else if (randomNumber1 > randomNumber2) {
+        selected.innerHTML = "Player1 Wins!"
+    } else if (randomNumber1 < randomNumber2) {
+        selected.innerHTML = "Player2 Wins!"
+    }
+}
+result();
+
