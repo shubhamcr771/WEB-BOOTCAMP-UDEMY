e08ecec6f6d189b8c126a77bf462d673944a4cbb
Shubham <shubhamsingh705010@gmail.com>
2023-10-22 16:34:05 +0530

    found the problem

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,3 +1,4 @@
 var randomNumber = Math.floor(Math.random*6+1);
 document.querySelector(".img1").setAttribute("src", "dice" + randomNumber + ".png");
 document.querySelector(".img2").setAttribute("src", "dice" + randomNumber + ".png");
+
