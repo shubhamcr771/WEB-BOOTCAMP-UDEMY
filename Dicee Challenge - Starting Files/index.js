71e93d17d0331b95b73725141391cec74b76b165
Shubham <shubhamsingh705010@gmail.com>
2023-10-22 16:50:40 +0530

    working

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,4 +1,5 @@
-var randomNumber = Math.floor(Math.random*6+1);
-document.querySelector(".img1").setAttribute("src", "dice" + randomNumber + ".png");
-document.querySelector(".img2").setAttribute("src", "dice" + randomNumber + ".png");
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
 
