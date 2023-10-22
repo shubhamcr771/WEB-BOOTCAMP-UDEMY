986f0c463583b41ef978104a84754c293955ca94
Shubham <shubhamsingh705010@gmail.com>
2023-10-22 16:57:09 +0530

    dice done

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,17 +1,20 @@
-var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+document
+  .querySelector(".img1")
+  .setAttribute("src", "dice" + randomNumber1 + ".png");
+document
+  .querySelector(".img2")
+  .setAttribute("src", "dice" + randomNumber2 + ".png");
 
 function result() {
-    var selected = document.querySelector("h1");
-    if (randomNumber1 === randomNumber2) {
-        selected.innerHTML = "Draw!";
-    } else if (randomNumber1 > randomNumber2) {
-        selected.innerHTML = "Player1 Wins!"
-    } else if (randomNumber1 < randomNumber2) {
-        selected.innerHTML = "Player2 Wins!"
-    }
+  var selected = document.querySelector("h1");
+  if (randomNumber1 === randomNumber2) {
+    selected.innerHTML = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    selected.innerHTML = "Player1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    selected.innerHTML = "Player2 Wins!";
+  }
 }
 result();
-
