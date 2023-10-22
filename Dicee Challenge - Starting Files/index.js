5414736bf513ac48017609c590d70e503549371d
Shubham <shubhamsingh705010@gmail.com>
2023-10-22 16:03:45 +0530

    fixed

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,6 +1,6 @@
 function diceroll() {
     var randomNumber1 = Math.floor(Math.random()*6+1);
-    document.getElementsByClassName("img1").setAttribute('src', 'dice' + randomNumber1 + '.png');
-    document.getElementsByClassName("img2").setAttribute('src', 'dice' + randomNumber1 + '.png');
+    document.getElementsByClassName("img1").setAttribute("src", "./dice3.png");
+    document.getElementsByClassName("img2").setAttribute("src", "./dice2.png");
 }
 diceroll();
