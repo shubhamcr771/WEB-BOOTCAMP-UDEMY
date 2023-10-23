a28b98d3d49b98cdff63342233585a8532160181
Shubham <shubhamsingh705010@gmail.com>
2023-10-23 16:47:49 +0530

    done

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,5 @@
 var noOfDrumButtons = document.querySelectorAll(".drum").length;
 
-
 for (var i = 0; i < noOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHtml = this.innerHTML;
@@ -9,54 +8,48 @@ for (var i = 0; i < noOfDrumButtons; i++) {
       case "d":
         var tom1 = new Audio("./sounds/tom-1.mp3");
         tom1.play();
-        
+
         break;
 
       case "j":
         var tom2 = new Audio("./sounds/tom-2.mp3");
         tom2.play();
-          
-        break;  
+
+        break;
 
       case "k":
         var tom3 = new Audio("./sounds/tom-3.mp3");
         tom3.play();
-          
-        break;  
+
+        break;
 
       case "l":
         var tom4 = new Audio("./sounds/tom-4.mp3");
         tom4.play();
-          
-        break; 
-      
+
+        break;
+
       case "w":
         var crash = new Audio("./sounds/crash.mp3");
         crash.play();
-          
-        break; 
-        
+
+        break;
+
       case "a":
         var kick = new Audio("./sounds/kick-bass.mp3");
         kick.play();
-          
-        break;  
+
+        break;
 
       case "s":
         var snare = new Audio("./sounds/snare.mp3");
         snare.play();
-            
+
         break;
-      
-    
-      default: 
-       console.log(buttonInnerHtml);
+
+      default:
+        console.log(buttonInnerHtml);
         break;
     }
-
-
-
-
-
   });
 }
