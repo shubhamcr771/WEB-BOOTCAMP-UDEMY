91b3b3c3ce80e1e8404d33e3c99b93fc2e6652cc
Shubham <shubhamsingh705010@gmail.com>
2023-10-23 11:01:51 +0530

    sound played

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,9 @@
 var noOfDrumButtons = document.querySelectorAll(".drum").length;
+var audio = new Audio('./sounds/tom-1.mp3');
+
 for (var i=0; i<noOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked");
+        audio.play();
     })
 }
 
