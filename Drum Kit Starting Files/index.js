22504a0443ecb5b63609993e4ae76489bb784232
Shubham <shubhamsingh705010@gmail.com>
2023-10-23 11:07:08 +0530

    formatted

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,9 +1,8 @@
 var noOfDrumButtons = document.querySelectorAll(".drum").length;
-var audio = new Audio('./sounds/tom-1.mp3');
+var audio = new Audio("./sounds/tom-1.mp3");
 
-for (var i=0; i<noOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        audio.play();
-    })
+for (var i = 0; i < noOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    audio.play();
+  });
 }
-
