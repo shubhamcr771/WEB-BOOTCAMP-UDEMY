b7e998eb236499e4922b4d3e771e1103303333c8
Shubham <shubhamsingh705010@gmail.com>
2023-10-25 12:39:22 +0530

    number added

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,5 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[randomNumber];
 
 function nextSequence() {
     var randomNumber = Math.floor((Math.random()*3)+1);
