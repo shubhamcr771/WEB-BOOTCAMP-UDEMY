e0090398bb2549e12d6e944b4300f3f5a7657904
Shubham <shubhamsingh705010@gmail.com>
2023-10-28 12:28:00 +0530

    done

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -3,9 +3,7 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
-
 function nextSequence() {
-
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColour = buttonColours[randomNumber];
@@ -15,18 +13,12 @@ function nextSequence() {
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-  
-  
-  $(".btn").click(function() {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
+  audio.play();
+
+  $(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
   });
-
-
 }
 
-nextSequence();  
-
-
-
+nextSequence();
