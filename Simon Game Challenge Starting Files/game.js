ea009d619a22e95e9edfa634889fa90744e5ed35
Shubham <shubhamsingh705010@gmail.com>
2023-10-28 13:54:38 +0530

    step-6 complete

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -3,25 +3,28 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
-
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
+
   animatePress(userChosenColour);
 });
 
+
+
 function playSound(name) {
-  
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
 function animatePress(currentColour) {
-  $().addClass("pressed");
+  $("#" + currentColour).addClass("pressed");
 
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
-
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -32,8 +35,6 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-
- 
+  
 }
-
 nextSequence();
