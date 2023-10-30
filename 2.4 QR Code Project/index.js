40dfedddd0dea004a2526e3494bb37e32fa477f4
Shubham <shubhamsingh705010@gmail.com>
2023-10-30 14:47:22 +0530

    path

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -23,7 +23,12 @@ inquirer
 
     const url = answers.URL;
     var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr-img.png"));
+
+    // HERE I AM STORING GENERATED QR IMAGE IN qr-image folder 
+    
+    const imagePath = path.join(__dirname, 'qr-image', 'qr-img.png');
+
+    qr_svg.pipe(fs.createWriteStream(imagePath));
 
     // 3 creating txt file to save the user input
 
