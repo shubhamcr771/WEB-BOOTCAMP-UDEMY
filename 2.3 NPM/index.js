3fd0791bc587100dd2f978368d9ec9c1f5a381ca
Shubham <shubhamsingh705010@gmail.com>
2023-10-30 08:02:32 +0530

    super hero

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -3,6 +3,19 @@
 // NOW THESE CODE ARE IN EMJS WHICH USED import heyword 
 
 import generateName from "sillyname";
+import superheroes from "superheroes";
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
+
+// SUPER HEROES NAME GENERATOR PACKAGE 
+
+import random from "superheroes";
+
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(`i am a ${superheroes.random()}`)
