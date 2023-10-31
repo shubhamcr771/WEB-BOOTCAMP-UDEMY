adf1d0f7f244745e4d7d9f205d4317d9eb80a846
Shubham <shubhamsingh705010@gmail.com>
2023-10-31 06:53:54 +0530

    http requests

--- Backend/3.3 Postman/index.js
+++ Backend/3.3 Postman/index.js
@@ -10,7 +10,7 @@ const port = 3000;
 // *********************
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+  res.send("<h1>Hello World I am learning node </h1>");
 });
 
 app.post("/register", (req, res) => {
