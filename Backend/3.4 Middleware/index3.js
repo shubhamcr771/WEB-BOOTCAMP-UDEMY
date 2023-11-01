76159eb70297e7d28aa2fd5d9ef3afc56ab1e3c4
Shubham <shubhamsingh705010@gmail.com>
2023-11-01 06:53:02 +0530

    request

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -1,12 +1,19 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Request Method:", req.method);
+  console.log("Request URL:", req.url);
+  next();
+  
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello I am learning custom middleware");
 });
 
 app.listen(port, () => {
