1cde5485561129ebbe85688847a6f9ef4b6537f9
Shubham <shubhamsingh705010@gmail.com>
2023-11-01 07:54:56 +0530

    half way

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,21 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import  bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+
+app.listen(port, () => {
+    console.log(`Listining on Port ${port}`);
+});  
