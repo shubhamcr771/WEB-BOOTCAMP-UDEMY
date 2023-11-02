e11a92de40b26abaaf3b1eb6d9041e2650bfa425
Shubham <shubhamsingh705010@gmail.com>
2023-11-02 07:33:19 +0530

    done

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -2,9 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, I am here!");
-});
+
 
 app.listen(3000, () => {
   console.log(`This is running on ${port}`);
