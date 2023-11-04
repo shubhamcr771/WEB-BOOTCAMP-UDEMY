8bb88c718ef905bc5a9829eda722f0041cae3c1b
Shubham <shubhamsingh705010@gmail.com>
2023-11-04 16:04:31 +0530

    done

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -7,14 +7,31 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+var authoriseduser = false;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
   });
 
+function passcheck(req, res, next) {
+    const password = req.body["password"];
+    if(password === "ILoveProgramming") {
+        authoriseduser = true;
+    }
+    next();
+    
+  }
+  app.use(passcheck);
+
+
 app.post("/check", (req, res) => {
-    res.sendFile(__dirname + "/public/secret.html");
+    if(authoriseduser) {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
   });
   
 app.listen(port, () => {
