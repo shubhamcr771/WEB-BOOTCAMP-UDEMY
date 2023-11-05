82107685441aabd7f5e62ec0bdd31da7fda64e37
Shubham <shubhamsingh705010@gmail.com>
2023-11-05 08:42:56 +0530

    done from my side

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -1,2 +1,27 @@
 import express from "express";
-import ejs from "ejs";
+const port = 3000;
+let app = express();
+
+app.set('view engine', 'ejs');
+const weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
+const weekend = ["Saturday", "Sunday"];
+const works = ["have fun", "work hard"];
+
+const d = new Date();
+let w_day = weekday[d.getDay()];
+let we_day = weekend[d.getDay()];
+
+function checkday(req, res, next) {
+    if(d.getDay() <= 4) {
+        res.render("index.ejs", {days: "weekday", work: works[1]});
+
+    } else {
+        res.render("index.ejs", {days: "weekend", work: works[0]});
+    }
+}
+
+app.use(checkday);
+
+
+
+app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
