9634278f0aa9fd9ac6e7dc6ad9680df311a707d9
Shubham <shubhamsingh705010@gmail.com>
2023-11-05 08:57:32 +0530

    ejs install

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -3,25 +3,23 @@ const port = 3000;
 let app = express();
 
 app.set('view engine', 'ejs');
-const weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
-const weekend = ["Saturday", "Sunday"];
-const works = ["have fun", "work hard"];
+
 
 const d = new Date();
-let w_day = weekday[d.getDay()];
-let we_day = weekend[d.getDay()];
+
 
 function checkday(req, res, next) {
     if(d.getDay() <= 4) {
-        res.render("index.ejs", {days: "weekday", work: works[1]});
+        res.render("index.ejs", {days: "weekday", work: "work hard"});
 
     } else if(d.getDay() > 4 || d.getDay() <= 6) {
-        res.render("index.ejs", {days: "weekend", work: works[0]});
+        res.render("index.ejs", {days: "weekend", work: "have fun"});
     }
+    next();
 }
 
 app.use(checkday);
 
 
 
-app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`This app listening on port ${port}!`));
