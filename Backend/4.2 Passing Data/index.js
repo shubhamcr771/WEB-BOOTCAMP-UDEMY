5c2784517513b6ed58c050f4bb50207a4f8a5543
Shubham <shubhamsingh705010@gmail.com>
2023-11-06 11:58:29 +0530

    done

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -4,27 +4,28 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {header: "Enter Your Name Here!"});
+  res.render("index.ejs", { header: "Enter Your Name Here!" });
 });
 // const { fName, lName } = req.body;
 // var fullName = `${fname} ${lName}`;
 // var characters = fullName.length;
 
 function check(req, res, next) {
-const { fName, lName } = req.body;
-var fullName = `${fName}${lName}`;
-var characters = fullName.length;
-if(characters >= 1) {
-  res.render("index.ejs", {header: `You Have ${characters} Characters in Your Name`});
-} else {
-  res.render("index.ejs", {header: "Enter Your Name"});
-}
+  const { fName, lName } = req.body;
+  var fullName = `${fName}${lName}`;
+  var characters = fullName.length;
+  if (characters >= 1) {
+    res.render("index.ejs", {
+      header: `You Have ${characters} Characters in Your Name`,
+    });
+  } else {
+    res.render("index.ejs", { header: "Enter Your Name" });
+  }
   next();
 }
 app.use(check);
