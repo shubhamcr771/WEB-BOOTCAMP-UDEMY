9abd87336c97ed6910f50e74df9c8e1c0396a8a6
Shubham <shubhamsingh705010@gmail.com>
2023-11-06 10:53:13 +0530

    stuck

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -7,12 +7,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile("/views/index.ejs");
+  req.sendFile("Backend\4.2 Passing Data\views\index.ejs");
 });
 
-app.post("/submit", (req, res) => {
-  res.render('/views/index.ejs');
-});
+app.post("/submit", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
