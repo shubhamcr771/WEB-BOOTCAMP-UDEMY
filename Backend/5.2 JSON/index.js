9f698ae84416572dcde8eb902f39ebd5acd8c4fd
Shubham <shubhamsingh705010@gmail.com>
2023-11-14 19:50:13 +0530

    json

--- Backend/5.2 JSON/index.js
+++ Backend/5.2 JSON/index.js
@@ -14,11 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let data;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { recipe:data });
+  res.render("index.ejs", { recipe: data });
 });
 
 app.post("/recipe", (req, res) => {
-
   switch (req.body.choice) {
     case "chicken":
       data = JSON.parse(recipeJSON)[0];
@@ -29,10 +28,10 @@ app.post("/recipe", (req, res) => {
     case "fish":
       data = JSON.parse(recipeJSON)[2];
       break;
-      default:
-      break;     
+    default:
+      break;
   }
-res.redirect("/");
+  res.redirect("/");
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
 });
