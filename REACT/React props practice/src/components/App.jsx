737607fd3c5927bc5b7adc4ac21858e47fda11ee
Shubham <shubhamsingh705010@gmail.com>
2023-11-30 13:49:31 +0530

    stuck

--- REACT/React props practice/src/components/App.jsx
+++ REACT/React props practice/src/components/App.jsx
@@ -1,23 +1,15 @@
 import React from "react";
+import contacts from "../contacts";
+
 
 function App() {
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
-    </div>
+    <contacts
+      name="Beyone"
+      imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      phone="+123 456 789"
+      email="b@beyonce.com"
+    />
   );
 }
 
--- REACT/React props practice/src/contacts.js
+++ REACT/React props practice/src/contacts.js
@@ -1,27 +1,42 @@
-const contacts = [
-  {
-    name: "Beyonce",
-    imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
-    email: "b@beyonce.com"
-  },
-  {
-    name: "Jack Bauer",
-    imgURL:
-      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com"
-  },
-  {
-    name: "Chuck Norris",
-    imgURL:
-      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
-  }
-];
+import React from "react";
 
 function contacts(props) {
-  return 
+  return <div>
+    <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
+  </div>
 }
+export default contacts;
+
+// const contacts = [
+//   {
+//     name: "Beyonce",
+//     imgURL:
+//       "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+//     phone: "+123 456 789",
+//     email: "b@beyonce.com"
+//   },
+//   {
+//     name: "Jack Bauer",
+//     imgURL:
+//       "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+//     phone: "+987 654 321",
+//     email: "jack@nowhere.com"
+//   },
+//   {
+//     name: "Chuck Norris",
+//     imgURL:
+//       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+//     phone: "+918 372 574",
+//     email: "gmail@chucknorris.com"
+//   }
+// ];
