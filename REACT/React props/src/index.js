a01806e93328f303792b41b3ca1573be9b5a34f0
Shubham <shubhamsingh705010@gmail.com>
2023-11-30 11:38:16 +0530

    props completed

--- REACT/React props/src/index.js
+++ REACT/React props/src/index.js
@@ -1,33 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+52 345,56657"
+      email="er@beondce.gmamilc"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
-
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://th.bing.com/th/id/OIP.ZtAmF1QdeGDmYGLzylTXnwHaJO?w=148&h=184&c=7&r=0&o=5&dpr=1.1&pid=1.7"
-      alt="avatar_img"
+    <Card
+      name="Jack Bauer"
+      img="https://th.bing.com/th/id/OIP.ZtAmF1QdeGDmYGLzylTXnwHaJO?w=148&h=184&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+      tel="+67 687 3478"
+      email="re@ddjfk.com"
     />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
+    <Card
+      name="Chuck Norris"
+      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      tel="+23 4546 678"
+      email="tr@jgldj.com"
     />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
   </div>,
   document.getElementById("root")
 );
+
