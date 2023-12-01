dfecab5bf1be518760e0c1906cd58c516f931808
Shubham <shubhamsingh705010@gmail.com>
2023-12-01 16:38:15 +0530

    data done

--- REACT/mapping-components-practice-forked/src/components/App.jsx
+++ REACT/mapping-components-practice-forked/src/components/App.jsx
@@ -10,45 +10,21 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-      <Entry/>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+        <Entry
+          emoji={emojipedia[0].emoji}
+          name={emojipedia[0].name}
+          meaning={emojipedia[0].meaning}
+        />
+        <Entry
+          emoji={emojipedia[1].emoji}
+          name={emojipedia[1].name}
+          meaning={emojipedia[1].meaning}
+        />
+        <Entry
+          emoji={emojipedia[2].emoji}
+          name={emojipedia[2].name}
+          meaning={emojipedia[2].meaning}
+        />
       </dl>
     </div>
   );
--- REACT/mapping-components-practice-forked/src/components/Entry.jsx
+++ REACT/mapping-components-practice-forked/src/components/Entry.jsx
@@ -5,13 +5,12 @@ function Entry(props) {
         <div className="term">
           <dt>
             <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
+              {props.emoji}
             </span>
-            <span>Tense Biceps</span>
+            <span>{props.name}</span>
           </dt>
           <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
+            {props.meaning}
           </dd>
         </div>
     );
