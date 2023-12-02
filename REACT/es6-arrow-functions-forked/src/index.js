4fa7aede2626b33e0f5a6d66f6cb3b4a0ae3b884
Shubham <shubhamsingh705010@gmail.com>
2023-12-02 16:01:38 +0530

    react arrow function

--- REACT/es6-arrow-functions-forked/src/index.js
+++ REACT/es6-arrow-functions-forked/src/index.js
@@ -4,29 +4,29 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+
+const newNumbers1 = numbers.map(x => x * 2);
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+
+const newNumbers2 = numbers.filter(num => num < 10);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+const newNumbers3 = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+
 
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+const newNumbers4 = numbers.find(num => num > 10);
 
 ////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+const newNumbers5 = numbers.findIndex(num =>  num > 10);
+
+console.log(newNumbers1)
+console.log(newNumbers2)
+console.log(newNumbers3)
+console.log(newNumbers4)
+console.log(newNumbers5)
+
