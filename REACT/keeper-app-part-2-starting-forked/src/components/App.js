65748b49ef53896d6a81f1e7c49263c24e993ac4
Shubham <shubhamsingh705010@gmail.com>
2023-12-04 11:01:08 +0530

    app done

--- REACT/keeper-app-part-2-starting-forked/src/components/App.js
+++ REACT/keeper-app-part-2-starting-forked/src/components/App.js
@@ -4,18 +4,17 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
 
-function createNotes(noteItem) {
- return <Note
- title={noteItem.title}
- content={noteItem.content}
-  />
-}
+
 
 function App() {
   return (
     <div>
       <Header />
-    {notes.map(createNotes)}
+    {notes.map((noteItem) => (<Note
+ title={noteItem.title}
+ content={noteItem.content}
+  />
+))}
       <Footer />
     </div>
   );
