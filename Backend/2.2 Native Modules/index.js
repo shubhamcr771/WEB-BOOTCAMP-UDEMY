4698369559851b99eb8e3a5972bc2e9c7265277e
Shubham <shubhamsingh705010@gmail.com>
2024-01-01 15:04:27 +0530

    backend revision

--- Backend/2.2 Native Modules/index.js
+++ Backend/2.2 Native Modules/index.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
-// fs.writeFile("message.txt", "Hello world",  (err) => {
-//     if (err) throw err;
-//     console.log("file had been saved!");
-// });
+fs.writeFile("message.txt", "Hello world", (err) => {
+  if (err) throw err;
+  console.log("file had been saved!");
+});
 
-fs.readFile("./message.txt", 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }); 
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+fs.writeFile("NewYear.txt", "Happy New Year", (err) => {
+  if (err) throw err;
+  console.log("Message has been done");
+});
+fs.readFile("./NewYear.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
