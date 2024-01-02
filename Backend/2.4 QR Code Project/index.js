9040c738e68c466bd1981def2b1d614559366c8a
Shubham <shubhamsingh705010@gmail.com>
2024-01-02 14:11:31 +0530

    qr-image

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -5,3 +5,25 @@
 */
 
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    { message: "What is your URL", name: "URL" },
+    var qr_svg = qr.image(url);
+
+    qr_svg.pipe(require("fs").createWriteStream("qr_img.png"));
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
